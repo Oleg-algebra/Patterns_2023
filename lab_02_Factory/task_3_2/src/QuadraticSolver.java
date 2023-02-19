111b3bd287235e338cbe1195e646d0edfc161b05
Oleg <sokol.wise@gmail.com>
2023-02-19 15:16:38 +0200

    task_3_2

--- lab_02_Factory/task_3_2/src/QuadraticSolver.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class QuadraticSolver implements Solver {
-    @Override
-    public Equation creatEquation(ArrayList<Double> coefficients) {
-        return new QuadraticEquation(coefficients);
-    }
-}
