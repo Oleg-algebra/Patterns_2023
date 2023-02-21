cdcca75815ded39675bba6b00e5133abb5a10776
Oleg <sokol.wise@gmail.com>
2023-02-21 10:25:44 +0200

    added task 3_2 correct solution

--- lab_02_Factory/task_3_2_1/src/BiquadraticSolver.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class BiquadraticSolver implements Solver {
-    @Override
-    public Equation creatEquation(ArrayList<Double> coefficients) {
-        return new BiQuadraticEquation(coefficients);
-    }
-}
--- lab_02_Factory/task_3_2_1/src/LinearSolver.java
+++
@@ -1,8 +0,0 @@
-import java.util.ArrayList;
-
-public class LinearSolver implements Solver {
-    @Override
-    public Equation creatEquation(ArrayList<Double> coefficients) {
-        return new Equation(coefficients);
-    }
-}
