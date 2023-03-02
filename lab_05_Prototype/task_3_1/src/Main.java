f98eb7cfe044d94ac329cd861480220cbd4ddbec
Oleg <sokol.wise@gmail.com>
2023-03-02 21:17:12 +0200

    commit

--- lab_05_Prototype/task_3_1/src/Main.java
+++ lab_05_Prototype/task_3_1/src/Main.java
@@ -1,6 +1,4 @@
-import java.io.FileNotFoundException;
-import java.io.PrintWriter;
-import java.security.spec.RSAOtherPrimeInfo;
+
 import java.util.ArrayList;
 
 public class Main {
@@ -13,20 +11,22 @@ public class Main {
 
         student.solveTask(fileInput.get(0));
 
-        ArrayList<Equation> copies = new ArrayList<>();
-        ArrayList<Equation> original = student.getOneRoot();
-        System.out.println("=======Original========");
-        for(Equation equation : original){
+        ArrayList<Equation> copies = new ArrayList<>();         // list of equation's copies
+        ArrayList<Equation> original = student.getOneRoot();    // list of original equations with one root
+        System.out.println("=======Original before test========");
+        System.out.println(original.get(0));        // print first original equation
+        for(Equation equation : original){          // make equation copies
             copies.add(equation.clone());
-            System.out.println(equation);
+//            System.out.println(equation);
         }
-        System.out.println("=======Copies=======");
-        for(Equation equation : copies){
-            System.out.println(equation);
-        }
+        System.out.println("=======Copies before test=======");
+        System.out.println(copies.get(0));
+//        for(Equation equation : copies){
+//            System.out.println(equation);
+//        }
 
-        System.out.println("==========Some test========");
-        original.get(0).setCoefficients(new ArrayList<>());   // delete coefficients of first original equation
+        System.out.println("==========After test========");          // we make some change in the first original equation
+        original.get(0).setCoefficients(new ArrayList<>());          // delete coefficients of first original equation
         System.out.println("Original: "+original.get(0));
         System.out.println("Copy: "+copies.get(0));
 
--- lab_05_Prototype/task_3_1/src/Solver.java
+++ lab_05_Prototype/task_3_1/src/Solver.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
 
 public interface Solver {
-    public Equation creatEquation(ArrayList<Double> coefficients);
+    Equation creatEquation(ArrayList<Double> coefficients);
 }
