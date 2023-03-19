84a04831ade054cb10b743e0cc111a86318aca63
Oleg <sokol.wise@gmail.com>
2023-03-19 22:10:28 +0200

    Facade implementation

---
+++ lab_11_Facade/task_3_1/src/UserConverter.java
@@ -0,0 +1,17 @@
+public class UserConverter {
+
+    public VideoFile convert(VideoFile videoFile, Codec destinationCodec){
+        System.out.println("\n======= VideoConversionFacade: conversion started. =======");
+        Codec sourceCodec = CodecFactory.extract(videoFile);
+
+        //Codec destinationCodec = new MPEG4CompressionCodec();
+        Buffer buffer = BitrateReader.read(videoFile, sourceCodec);
+        VideoFile videoFileConverted = BitrateReader.convert(buffer, videoFile.getName(), destinationCodec);
+        AudioMixer audioMixer = new AudioMixer();
+        audioMixer.fix(videoFileConverted, videoFile.getAudioBuffer());
+
+        System.out.println("====== VideoConversionFacade: conversion completed =======\n");
+        return videoFileConverted;
+    }
+
+}
